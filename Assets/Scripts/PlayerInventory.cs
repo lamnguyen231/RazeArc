ced170cfdaf32a541085167d5b7073ea0a639491
lamnguyen231 <mysteryme2312@gmail.com>
2026-02-22 12:11:34 +0700

    add acid/lava

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class PlayerInventory : MonoBehaviour
+public class PlayerInventory : MonoBehaviour, IDamageable
 {
     [Header("Stats")]
-    public int playerHealth;
+    public float playerHealth = 100;
+    public float playerMaxHealth = 100;
+
+    [Header("UI")]
+    public TextMeshProUGUI healthText;
 
     [Header("Keycards")]
     public bool hasRedKey = false;
     public bool hasBlueKey = false;
     public bool hasGreenKey = false;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateHealthUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
     public void PickUpKey(string keyColor)
     {
         if (keyColor == "Red") hasRedKey = true;
@@ -19,15 +35,25 @@ public class PlayerInventory : MonoBehaviour
         else if (keyColor == "Green") hasGreenKey = true;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void TakeDamage(float damageAmount)
     {
-        
+        playerHealth -= damageAmount;
+        Debug.Log($"You have taken ${damageAmount} amount of damage.");
+
+        UpdateHealthUI();
+
+        if (playerHealth <= 0)
+        {
+            Debug.Log("You are ded.");
+            //SceneManager.LoadScene("GameOverRetry");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateHealthUI()
     {
-        
+        if (healthText != null)
+        {
+            healthText.text = "Health: " + playerHealth;
+        }
     }
 }
